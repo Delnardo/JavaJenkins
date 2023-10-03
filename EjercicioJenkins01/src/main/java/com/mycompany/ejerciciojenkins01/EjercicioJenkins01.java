f3f47acc8027351328e07d6d5a370fac5a22018f
Alfredo J.R. Delnardo <alfredodelnardo@gmail.com>
2023-10-02 21:51:32 -0300

    Test jenkins 02

--- EjercicioJenkins01/src/main/java/com/mycompany/ejerciciojenkins01/EjercicioJenkins01.java
+++ EjercicioJenkins01/src/main/java/com/mycompany/ejerciciojenkins01/EjercicioJenkins01.java
@@ -12,5 +12,6 @@ public class EjercicioJenkins01 {
 
     public static void main(String[] args) {
         System.out.println("Hello World! By AD");
+        System.out.println("Test Jenkins");
     }
 }
